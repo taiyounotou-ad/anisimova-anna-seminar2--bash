dc74823531edab3a2a5cd3ad493c402640796311
Anisimova Anna <keithrelf13@gmail.com>
2025-11-01 22:19:06 +0300

    Исправлена ошибка в файле script5.sh

--- Семинар 22-23.10/script5.sh
+++ Семинар 22-23.10/script5.sh
@@ -5,7 +5,10 @@ for files in "$DIR"/*; do
 if [ -f "$files" ]; then
 FILE_NAME=$(basename "$files")
 FILE_NAME_LOWER=$(echo "$FILE_NAME" | tr '[:upper:]' '[:lower:]')
-mv "$FILE_NAME" "$DIR/$FILE_NAME_LOWER"
+if [[ "$FILE_NAME" != "$FILE_NAME_LOWER" ]]; then
+mv "$files" "$DIR/$FILE_NAME_LOWER"
+echo "Переименован: '$FILE_NAME' -> '$FILE_NAME_LOWER'"
+fi
 fi
 done
 echo "Все файлы переименованы."
